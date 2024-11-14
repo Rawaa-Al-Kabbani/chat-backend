962e4b9b44836f2d5ab1fe2f62515244d06f612d
Rawaa-Al-Kabbani <rawaa.alkabbani@outlook.com>
2024-11-14 13:47:24 +0100

    Use ConfigService when setting up Gateway

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -2,17 +2,6 @@ import { OnModuleInit } from '@nestjs/common';
 import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
-const SOCKET_PORT = process.env.SOCKET_PORT
-  ? parseInt(process.env.SOCKET_PORT, 10)
-  : 5001;
-
-const CORS_ORIGIN = process.env.CORS_ORIGIN ?? 'http://localhost:3000';
-
-@WebSocketGateway(SOCKET_PORT, {
-  cors: {
-    origin: [CORS_ORIGIN],
-  },
-})
 export class GatewayService implements OnModuleInit {
   @WebSocketServer()
   server: Server;
